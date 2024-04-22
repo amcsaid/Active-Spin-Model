143ae9f1effc6518c9d9b757cfad4a3f8de1e4e7
zakarya <zakaria.elkhiyati@gmail.com>
2024-04-22 11:23:43 +0200

    Refactor compute_delta method in rates.py to handle different event types

--- lvmc/core/rates.py
+++ lvmc/core/rates.py
@@ -54,6 +54,52 @@ class RatesManager:
             padded_particles, kernel, padding=0, groups=2
         ).permute(1, 2, 0)
 
+    def compute_energies(self) -> torch.Tensor:
+        """
+        Compute the energy per site
+        :return: Tensor of shape (lattice.height, lattice.width)
+        """
+        energies = -torch.sum(self.interaction_forces * self.lattice.particles, dim=2)
+        return energies
+
+    @property
+    def total_energy(self) -> torch.Tensor:
+        """
+        Compute the total energy of the lattice
+        :return: The total energy of the lattice
+        """
+        return torch.sum(self.compute_energies())
+
+    def compute_delta(self, event_type: EventType) -> torch.Tensor:
+        """
+        Compute the change in energy for each event type
+        :param event_type: The type of event to compute the change in energy for
+        :return: The change in energy for each site for the given event type.
+        """
+        if isinstance(event_type, int):
+            event_type = EventType(event_type)
+        if event_type == EventType.ROTATE:
+            return self.compute_rotate_delta()
+
+        if event_type == EventType.HOP:
+            return self.compute_hop_delta()
+
+        if event_type == EventType.FLIP:
+            return -4 * self.energies
+
+        if event_type == EventType.ROTATE_NEG:
+            return -self.compute_rotate_delta() - 4 * self.energies
+
+    def compute_rotate_delta(self) -> torch.Tensor:
+        """
+        Compute the change in energy for a rotation event
+        :return: The change in energy for each site for a rotation event
+        """
+        rotation_matrix = torch.tensor([[0, 1], [-1, 0]], dtype=torch.int8)
+        rotated_particles = torch.matmul(self.lattice.particles, rotation_matrix)
+        H_ortho = -torch.sum(self.interaction_forces * rotated_particles, dim=2)
+        return 2 * (H_ortho - self.energies) + self.occupancy_deltas
+
 
 
 if __name__ == "__main__":
