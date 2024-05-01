90262a1c9de9b1cfe6870e9731b730157516af95
zakarya <zakaria.elkhiyati@gmail.com>
2024-05-01 16:37:41 +0200

    Update parameters

--- examples/parameters.py
+++ examples/parameters.py
@@ -1,21 +1,13 @@
 import torch
 
 # Parameters for ParticleLattice
-width = 50
-height = 25
+width = 49
+height = 49
 density = 0.3
 
 # Simulation parameters
 g = 2.0  # Alignment sensitivity
 v0 = 100.0  # Base transition rate
-
-# Flow parameters
-flow_params = {
-    # include relevant parameters here
-    "type": "Poiseuille",
-    "v1": 100,
-}
-
 
 
 obstacles = torch.zeros((height, width), dtype=torch.bool)
